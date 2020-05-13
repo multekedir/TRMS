cc1bd18885b7413fe7e89cf25bc898452dc02e3c
Multezem Kedir <kedirmultezem@gmail.com>
2020-05-13 06:51:04 -0700

    loaded index page servlet
    Signed-off-by: Multezem Kedir <multekedir@users.noreply.github.com>

--- src/main/java/com/revature/servlet/Home.java
+++ src/main/java/com/revature/servlet/Home.java
@@ -1,19 +1,28 @@
 package com.revature.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+@WebServlet("/")
 public class Home extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String htmlResponse = "<html><h3>Hello<h3></html>";
+        String name = req.getParameter("name");
+        String htmlResponse = "<html><h3>Hello " + name + "<h3></html>";
         PrintWriter writer = resp.getWriter();
-        writer.write(htmlResponse);
+        String filePath = req.getServletContext().getRealPath("index.html");
+        String contents = new String(Files.readAllBytes(Paths.get(filePath)));
+        writer.write(contents);
     }
+
+
 }
 
