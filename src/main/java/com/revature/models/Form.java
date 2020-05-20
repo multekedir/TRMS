73c76c93e229bae160f7c4a8658710eb94f949df
Multezem Kedir <multekedir@users.noreply.github.com>
2020-05-19 19:51:56 -0700

    able to insert form in to db
    Signed-off-by: Multezem Kedir <multekedir@users.noreply.github.com>

---
+++ src/main/java/com/revature/models/Form.java
@@ -0,0 +1,161 @@
+package com.revature.models;
+
+import com.revature.services.EmployeeService;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Form {
+    Status status;
+    private int id;
+    private double amount;
+    private Employee submittedBY;
+    private String description;
+
+    public Form() {
+    }
+
+    /**
+     * @param amount
+     * @param submittedBY
+     * @param description
+     */
+    public Form(double amount, Employee submittedBY, String description) {
+        this.amount = amount;
+        this.submittedBY = submittedBY;
+        this.description = description;
+    }
+
+    /**
+     * @param rs
+     */
+    public Form(ResultSet rs) throws SQLException {
+        this.setSubmittedBY(EmployeeService.getEmployeeByID(rs.getInt("submitted_by".toUpperCase())));
+        this.setAmount(rs.getDouble("AMOUNT"));
+        this.setDescription("description".toUpperCase());
+        this.setStatus(Status.valueOf(rs.getString("waiting_for".toUpperCase())));
+        this.setId(rs.getInt("ID"));
+    }
+
+    /**
+     * @return
+     */
+    public String benCOApprove() {
+        status = Status.BENCO;
+        return String.valueOf(status);
+    }
+
+    /**
+     * @return
+     */
+    public String headApprove() {
+        status = Status.HEAD;
+        return String.valueOf(status);
+    }
+
+    /**
+     * Gets amount.
+     *
+     * @return Value of amount.
+     */
+    public double getAmount() {
+        return amount;
+    }
+
+    /**
+     * Sets new amount.
+     *
+     * @param amount New value of amount.
+     */
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * Gets description.
+     *
+     * @return Value of description.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets new description.
+     *
+     * @param description New value of description.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Gets submittedBY.
+     *
+     * @return Value of submittedBY.
+     */
+    public Employee getSubmittedBY() {
+        return submittedBY;
+    }
+
+    /**
+     * Sets new submittedBY.
+     *
+     * @param submittedBY New value of submittedBY.
+     */
+    public void setSubmittedBY(Employee submittedBY) {
+        this.submittedBY = submittedBY;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return Value of id.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets new id.
+     *
+     * @param id New value of id.
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets status.
+     *
+     * @return Value of status.
+     */
+    public String getStatus() {
+        return String.valueOf(status);
+    }
+
+    /**
+     * Sets new status.
+     *
+     * @param status New value of status.
+     */
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Form{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", submittedBY=" + submittedBY +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+
+    private enum Status {
+        BENCO,
+        HEAD,
+    }
+}
--- src/main/resources/SQL/tables.sql
+++ src/main/resources/SQL/tables.sql
@@ -15,7 +15,6 @@ DROP USER TRMS CASCADE;
 
 conn TRMS/p4ssw0rd;
 
-
 create table DEPARTMENTS
 (
     ID   NUMBER generated as identity,
@@ -36,64 +35,10 @@ alter table DEPARTMENTS
         primary key (ID)
 /
 
-create table EMPLOYEES
-(
-    ID         NUMBER generated as identity,
-    USER_NAME  VARCHAR2(45) not null,
-    FIRST_NAME VARCHAR2(40) not null,
-    LAST_NAME  VARCHAR2(40) not null,
-    PASSWORD   VARCHAR2(80) not null,
-    DEPARTMENT NUMBER       not null,
-    ROLE       NUMBER,
-    constraint DEPARTMENT
-        foreign key (DEPARTMENT) references DEPARTMENTS
-            on delete set null
-)
-/
-
-create unique index EMPLOYEES_ID_UINDEX
-    on EMPLOYEES (ID)
-/
-
-create unique index EMPLOYEES_USER_NAME_UINDEX
-    on EMPLOYEES (USER_NAME)
-/
-
-alter table EMPLOYEES
-    add constraint EMPLOYEES_PK
-        primary key (ID)
-/
-
-create table DEPT_MANAGER
-(
-    ID            NUMBER generated as identity,
-    DEPARTMENT_ID NUMBER,
-    MANAGER_ID    NUMBER,
-    constraint DEPARTMENT_ID
-        foreign key (DEPARTMENT_ID) references DEPARTMENTS
-            on delete cascade,
-    constraint MANAGER_ID
-        foreign key (MANAGER_ID) references EMPLOYEES
-            on delete set null
-)
-/
-
-create unique index DEPT_MANAGER_ID_UINDEX
-    on DEPT_MANAGER (ID)
-/
-
-alter table DEPT_MANAGER
-    add constraint DEPT_MANAGER_PK
-        primary key (ID)
-/
-
 create table ROLE
 (
-    ID         NUMBER generated as identity,
-    ROLE_NAME  VARCHAR2(20) not null,
-    SUPERVISOR NUMBER       not null,
-    constraint ROLE_EMPLOYEES_ID_FK
-        foreign key (SUPERVISOR) references EMPLOYEES
+    ID        NUMBER generated as identity,
+    ROLE_NAME VARCHAR2(20) not null
 )
 /
 
@@ -110,10 +55,94 @@ alter table ROLE
         primary key (ID)
 /
 
-alter table EMPLOYEES
-    add constraint ROLE_ID
-        foreign key (ROLE) references TRMS.ROLE
+create table EMPLOYEES
+(
+    ID         NUMBER generated as identity,
+    FIRST_NAME VARCHAR2(40) not null,
+    LAST_NAME  VARCHAR2(40) not null,
+    DEPARTMENT NUMBER default null,
+    ROLE       NUMBER default null,
+    USERNAME   VARCHAR2(40) not null,
+    PASSWORD   VARCHAR2(60) not null,
+    constraint DEPARTMENT
+        foreign key (DEPARTMENT) references DEPARTMENTS
+            on delete set null,
+    constraint ROLE_ID
+        foreign key (ROLE) references ROLE
             on delete set null
+)
 /
 
+create unique index EMPLOYEES_ID_UINDEX
+    on EMPLOYEES (ID)
+/
+
+create unique index EMPLOYEES_USERNAME_UINDEX
+    on EMPLOYEES (USERNAME)
+/
+
+alter table EMPLOYEES
+    add constraint EMPLOYEES_PK
+        primary key (ID)
+/
+
+create table DEP_MANAGER
+(
+    DEPARTMENT NUMBER,
+    MANAGER    NUMBER,
+    ID         NUMBER generated as identity,
+    constraint DEP_MANAGER_DEPARTMENTS_ID_FK
+        foreign key (DEPARTMENT) references DEPARTMENTS,
+    constraint DEP_MANAGER_EMPLOYEES_ID_FK
+        foreign key (MANAGER) references EMPLOYEES
+)
+/
+
+create unique index DEP_MANAGER_ID_UINDEX
+    on DEP_MANAGER (ID)
+/
+
+alter table DEP_MANAGER
+    add constraint DEP_MANAGER_PK
+        primary key (ID)
+/
+
+create table ROLE_SUPER
+(
+    ID         NUMBER generated as identity,
+    ROLE       NUMBER,
+    SUPERVISOR NUMBER,
+    constraint ROLE_SUPER_EMPLOYEES_ID_FK
+        foreign key (SUPERVISOR) references EMPLOYEES,
+    constraint ROLE_SUPER_ROLE_ID_FK
+        foreign key (ROLE) references ROLE
+)
+/
+
+create unique index ROLE_SUPER_ID_UINDEX
+    on ROLE_SUPER (ID)
+/
+
+alter table ROLE_SUPER
+    add constraint ROLE_SUPER_PK
+        primary key (ID)
+/
+
+create table FORMS
+(
+    ID           NUMBER generated as identity,
+    SUBMITTED_BY NUMBER                    not null,
+    SUBMITTED_ON DATE default CURRENT_DATE not null,
+    AMOUNT       NUMBER(6, 2)              not null,
+    WAITING_FOR  VARCHAR2(20),
+    DESCRIPTION  NUMBER,
+    constraint FORMS_EMPLOYEES_ID_FK
+        foreign key (SUBMITTED_BY) references EMPLOYEES
+            on delete cascade
+)
+/
+
+create unique index FORMS_ID_UINDEX
+    on FORMS (ID)
+/
 
